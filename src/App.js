17154705038841d873959c1851e82a3ce150ce15
Ananya Pati <126486682+ananyapati26@users.noreply.github.com>
2024-05-19 16:37:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
   return (
     
     <div className="App" >
-      {/* <canvas ref={canvasRef} style={{ width: '100vw', height: '100vh', zIndex: '-1000'}}> */}
+      {/* <canvas ref={canvasRef} style={{ width: '100vw', height: '100vh'}}> */}
 
       <Header />
       <HeroSection />
